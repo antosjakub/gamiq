ad4d7594990ebb4a9cbc25807c794cc8697e96c8
Jakub Antos <antos_j@outlook.com>
2025-08-15 23:41:03 +0200

    moving cart

--- vis.py
+++ vis.py
@@ -21,7 +21,6 @@ SCREEN_H = 600
 
 # Set up the game window
 window = pygame.display.set_mode((SCREEN_W, SCREEN_H))
-window.fill(NIGGA)
 pygame.display.set_caption("Cartpole")
 
 
@@ -35,39 +34,45 @@ cartpole_arm_w = 300
 cartpole_arm_h = 15
 cartpole_init_arm_angle = 10
 
+x = 0
+
 # Game loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    # cartpole base
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                x += 5
+            if event.key == pygame.K_LEFT:
+                x -= 5
+    
+    # GRAPHICS
+    pygame.display.flip()
+    window.fill(NIGGA)
+    # -- cartpole base
     pygame.draw.rect(window, WHITE, [
-        SCREEN_W//2 - cartpole_w//2,
+        SCREEN_W//2 - cartpole_w//2 + x,
         SCREEN_H//2 - cartpole_h//2 + ground_h_offset,
         cartpole_w,
         cartpole_h
     ], 0)
-    # cartpole arm
+    # -- cartpole arm
     arm_pnts = [
-        #Vector2(cartpole_arm_w//2*i, cartpole_arm_h//2*j)
         Vector2(cartpole_arm_w*i, cartpole_arm_h*j)
-        #for i,j in [[1,1], [1,-1], [-1,-1], [-1,1]]
-        #for i,j in [[1,1], [1,0], [0,0], [0,1]]
         for i,j in [[1,1/2], [1,-1/2], [0,-1/2], [0,1/2]]
     ] 
     arm_coords = [
-        Vector2(SCREEN_W//2, SCREEN_H//2 + ground_h_offset) +
+        Vector2(SCREEN_W//2 + x, SCREEN_H//2 + ground_h_offset) +
         p.rotate(-90+cartpole_init_arm_angle) for p in arm_pnts
     ]
     pygame.draw.polygon(window, WHITE, arm_coords, 0)
-    # cartpole center
+    # -- cartpole center
     pygame.draw.circle(window, NIGGA, [
-        SCREEN_W//2,
+        SCREEN_W//2 + x,
         SCREEN_H//2 + ground_h_offset
     ], 5, 0)
-
-
 
     pygame.display.update()
 
