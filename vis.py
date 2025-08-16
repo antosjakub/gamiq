36a36d4e02b159e03a5146463c39c06e5adc39b6
Jakub Antos <antos_j@outlook.com>
2025-08-16 13:20:37 +0200

    physically realistic pendulum

--- vis.py
+++ vis.py
@@ -2,6 +2,10 @@ import pygame
 from pygame.math import Vector2
 import numpy as np
 
+def deg_from_rad(rad):
+    return rad/(2*np.pi) * 360
+def rad_from_deg(deg):
+    return deg/360 * 2*np.pi
 
 def get_circle_pos(theta):
     return np.array([np.sin(theta), np.cos(theta)])
@@ -22,26 +26,32 @@ SCREEN_H = 600
 # Set up the game window
 window = pygame.display.set_mode((SCREEN_W, SCREEN_H))
 pygame.display.set_caption("Cartpole")
+clock = pygame.time.Clock()
 
 
 ground_h_offset = 150
-pygame.draw.line(window, WHITE, Vector2(0,SCREEN_H//2+ground_h_offset), Vector2(SCREEN_W,SCREEN_H//2+ground_h_offset), 1)
 
 cartpole_w = 200
 cartpole_h = 20
 
 cartpole_arm_w = 300
 cartpole_arm_h = 15
-cartpole_init_arm_angle = 10
 
+# initial conditions
+theta = rad_from_deg(10)
+theta_dot = 0
 x = 0
-theta = cartpole_init_arm_angle
 
-def rad_to_deg(rad):
-    return rad/(2*np.pi) * 360
-def deg_to_rad(deg):
-    return deg/360 * 2*np.pi
+# constants
+l = cartpole_arm_w
+g = 1
 
+def h_from_theta(theta):
+    return l*np.cos(theta)
+def v_from_E_and_theta(E, theta):
+    return np.sqrt(2*(E-g*l*np.cos(theta)))
+
+i = 0
 # Game loop
 running = True
 while running:
@@ -51,18 +61,21 @@ while running:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
                 x += 5
-                theta -= 5
+                theta -= rad_from_deg(10)
             if event.key == pygame.K_LEFT:
                 x -= 5
-                theta += 5
+                theta += rad_from_deg(10)
+    dt = clock.tick(100)
     
-    alpha = rad_to_deg( np.arctan( 1/cartpole_arm_w * np.sin(deg_to_rad(theta)) ) )
-    print(theta, alpha)
-    theta += alpha
+    delta_t = dt/500
+    theta_double_dot = g * np.sin(theta)
+    theta_dot += theta_double_dot * delta_t
+    theta += theta_dot * delta_t
     
     # GRAPHICS
     pygame.display.flip()
     window.fill(NIGGA)
+    pygame.draw.line(window, WHITE, Vector2(0,SCREEN_H//2+ground_h_offset), Vector2(SCREEN_W,SCREEN_H//2+ground_h_offset), 1)
     # -- cartpole base
     pygame.draw.rect(window, WHITE, [
         SCREEN_W//2 - cartpole_w//2 + x,
@@ -77,7 +90,7 @@ while running:
     ] 
     arm_coords = [
         Vector2(SCREEN_W//2 + x, SCREEN_H//2 + ground_h_offset) +
-        p.rotate(-90+theta) for p in arm_pnts
+        p.rotate(-90+deg_from_rad(theta)) for p in arm_pnts
     ]
     pygame.draw.polygon(window, WHITE, arm_coords, 0)
     # -- cartpole center
@@ -87,6 +100,10 @@ while running:
     ], 5, 0)
 
     pygame.display.update()
+
+    i+=1
+    #if i > 100:
+    #    break
 
 # Quit Pygame
 pygame.quit()
